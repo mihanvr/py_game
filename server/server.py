9ad90c48e6f2aa770bda1948ce624e6473da1db5
miha <mihanvr@gmail.com>
2020-06-30 19:50:43 +0300

    add BaseRepositoryEndpoint

--- server/server.py
+++ server/server.py
@@ -1,7 +1,21 @@
 from flask import Flask, request, jsonify
+
 from battle import test_class, database
+from battle.database import BaseRepository
 
 app = Flask(__name__)
+
+
+class BaseRepositoryEndpoints:
+    repository: BaseRepository
+
+    def __init__(self, path: str, app: Flask, repository: BaseRepository):
+        self.repository = repository
+        app.add_url_rule(path, self.get_all, methods=['GET'])
+
+
+    def get_all(self):
+        return jsonify(self.repository.get_all())
 
 
 @app.route('/command/<string:command>')
@@ -21,17 +35,17 @@ def index():
     return get_command('look')
 
 
-@app.route('/weapons', methods=['GET'])
+# @app.route('/weapons', methods=['GET'])
 def get_weapons():
     return jsonify(database.weapon_repository.get_all())
 
 
-@app.route('/weapons/<string:id>', methods=['GET'])
+# @app.route('/weapons/<string:id>', methods=['GET'])
 def get_weapons_by_id(id: str):
     return jsonify(database.weapon_repository.get_one(id))
 
 
-@app.route('/weapons', methods=['POST'])
+# @app.route('/weapons', methods=['POST'])
 def create_weapon():
     data = request.json
     database.weapon_repository.create_one(data)
@@ -39,7 +53,7 @@ def create_weapon():
     return 'Weapons created'
 
 
-@app.route('/weapons', methods=['PUT'])
+# @app.route('/weapons', methods=['PUT'])
 def update_weapon():
     data = request.json
     database.weapon_repository.update_one(data)
@@ -47,13 +61,19 @@ def update_weapon():
     return 'Weapons created'
 
 
-@app.route('/weapons/<string:id>', methods=['DELETE'])
+# @app.route('/weapons/<string:id>', methods=['DELETE'])
 def delete_weapon(id: str):
     if database.weapon_repository.delete_one(id) is not None:
         return jsonify(success=True)
     else:
         return jsonify(success=False)
 
+
+# app.route('/weapons', endpoint=get_weapons, methods=['GET'])
+# app.route('/weapons/<string:id>', endpoint=get_weapons_by_id, methods=['GET'])
+
+BaseRepositoryEndpoints('/weapons', app, database.weapon_repository)
+BaseRepositoryEndpoints('/spells', app, database.spell_repository)
 
 if __name__ == '__main__':
     app.run(debug=True)
